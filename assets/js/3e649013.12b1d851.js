e80e547cb92b20b8fa9b07ef14de9a18548f6b65
let <oof.exe.lol@gmail.com>
2024-05-12 15:10:08 -0300

    Built and published by Moonwave

---
+++ assets/js/3e649013.12b1d851.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[395],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/lettuce-magician/Stir/blob/main","baseUrl":"/Stir/","classOrder":["Stir","Form"],"apiCategories":[]}')}}]);
---
+++ assets/js/6830fa90.330ade3e.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[490],{15745:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-pages","id":"default"}')}}]);
